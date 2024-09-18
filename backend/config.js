af8649b80b0781e08194eb82b776369640131f0c
p0nyo <boltyourfaceoff@gmail.com>
2024-09-18 20:25:28 +1200

    first commit

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://tsentreyson:7958zqZ3@cluster0.0ydo3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+
---
+++ frontend/src/pages/CreateBooks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 
+
+
+const CreateBooks = () => {
+  return (
+    <div>CreateBooks</div>
+  )
+}
+
+export default CreateBooks;
